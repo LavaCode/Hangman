e37e3a58c47a783f9e020120d71086ec858ada39
LavaCode <kasper.zegel@gmail.com>
2021-02-01 08:04:42 +0100

    First solution upload

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
 const { question } = require("readline-sync");
 const { displayWordSoFar, isGameWon, isGameLost } = require("./gamelogic");
 
+// voer hieronder het te raden woord in
+const giveWordToGuess = "Hallo";
+const technicalWord = giveWordToGuess.toLowerCase();
+
 function game(word, guesses) {
-  console.log("Dit heb je tot nu toe geraden: ", guesses);
+  console.log("Dit heb je tot nu toe geraden: " + displayWordSoFar(word, guesses));
 
   const letter = question("Raad een letter: ");
 
+  if(!isNaN(letter)) {
+    console.log("Fout, voer geen getal in. Alleen een letter is toegestaan.");
+    return;
+  } else if(letter.length > 1){
+    console.log("Fout, geef maximaal 1 karakter in!");
+    return;
+  }
   // voeg de geraden letter toe aan de array met guesses
-  guesses.push(letter);
+  const input = letter.toLowerCase();
+  guesses.push(input);
 
+  if(isGameWon(word, guesses)){
+    console.log("\nAwesome! je hebt gewonnen, het woord was: " + word);
+    return;
+  }
+  else if(isGameLost(word, guesses)){
+    console.log("\nHelaas je hebt verloren!");
+    return;
+  }
   // volgende ronde! we roepen game nog een keer aan
   game(word, guesses);
 }
 
 console.log(`
+Welkom bij.....
+
 __________  
 | /     |    ░██████╗░░█████╗░██╗░░░░░░██████╗░░░░░░██╗███████╗
 |/     _o_   ██╔════╝░██╔══██╗██║░░░░░██╔════╝░░░░░░██║██╔════╝
 |       O    ██║░░██╗░███████║██║░░░░░██║░░██╗░░░░░░██║█████╗░░
 |      / \\   ██║░░╚██╗██╔══██║██║░░░░░██║░░╚██╗██╗░░██║██╔══╝░░
 |            ╚██████╔╝██║░░██║███████╗╚██████╔╝╚█████╔╝███████╗
-===========  ░╚═════╝░╚═╝░░╚═╝╚══════╝░╚═════╝░░╚════╝░╚══════╝
+===========  ░╚═════╝░╚═╝░░╚═╝╚══════╝░╚═════╝░░╚════╝np░╚══════╝
 `);
 
-game("javascript", []);
+game(technicalWord, []);
+ 
